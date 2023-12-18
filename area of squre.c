45fd9c2a9cb91d428ecc0fc99bacbacb34800274
Nikhillokesh777 <154219009+Nikhillokesh777@users.noreply.github.com>
2023-12-18 15:27:52 +0530

    Add files via upload

---
+++ area of squre.c
@@ -0,0 +1,14 @@
+//area of squre
+#include<stdio.h>
+int main()
+{
+	float  length,area;
+	printf("enter the length");
+	scanf("%f",&length);
+	area = 6*length;
+	printf("area:%f",area);
+	
+	return 0;
+	
+	
+}
